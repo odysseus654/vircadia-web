f689495f5a25ebdfbc3fddd8a519e066db3d4635
Heather Anderson <heath@odysseus.anderson.name>
2021-05-13 22:14:18 -0700

    attempt to get mouse behaviors working

--- src/components/overlays/OverlayShell.vue
+++ src/components/overlays/OverlayShell.vue
@@ -11,8 +11,6 @@
 <style lang="scss" scoped>
     .outer {
         position: absolute;
-        left: 400px;
-        top: 200px;
     }
 
     div.title {
@@ -20,28 +18,58 @@
         white-space: nowrap;
         text-overflow: ellipsis;
     }
+
+    div.resize {
+        position: absolute;
+        opacity: 0;
+    }
+    div.resize-top, div.resize-nw, div.resize-ne, div.resize-bottom, div.resize-sw, div.resize-se {
+        height: 15px;
+    }
+    div.resize-top, div.resize-nw, div.resize-ne {
+        top: -10px;
+    }
+    div.resize-bottom, div.resize-sw, div.resize-se {
+        bottom: -10px;
+    }
+    div.resize-top, div.resize-bottom {
+        width: 100%;
+        cursor: ns-resize;
+    }
+    div.resize-left, div.resize-nw, div.resize-ne, div.resize-right, div.resize-sw, div.resize-se {
+        width: 15px;
+    }
+    div.resize-left, div.resize-nw, div.resize-sw {
+        left: -10px;
+    }
+    div.resize-right, div.resize-ne, div.resize-se {
+        right: -10px;
+    }
+    div.resize-left, div.resize-right {
+        height: 100%;
+        cursor: ew-resize;
+    }
+    div.resize-nw, div.resize-se {
+        cursor: nwse-resize;
+    }
+    div.resize-ne, div.resize-sw {
+        cursor: nesw-resize;
+    }
 </style>
 
 <template>
     <q-card
-        class="outer no-wrap items-stretch"
-        ref="overlayCard"
-        @mouseover="onHoverStart"
-        @mouseleave="onHoverEnd"
+        class="outer column no-wrap items-stretch"
+        @mouseenter="hovered=true"
+        @mouseleave="hovered=false"
+        @mousedown="$emit('overlay-action', 'select')"
         :style="{
             // Dimensions
-            height: overlayHeight + 'px', // Should these two be a string so that we can define vh or whatever at will?
-            width: overlayWidth + 'px',
+            height: height + 'px', // Should these two be a string so that we can define vh or whatever at will?
+            width: width + 'px',
             // Positioning
-            position: overlayPosition,
-            top: overlayTop,
-            bottom: overlayBottom,
-            right: overlayRight,
-            left: overlayLeft,
-            // Styling
-            background: overlayBackground,
-            border: overlayBorder,
-            'box-shadow': overlayBoxShadow
+            top: top + 'px',
+            left: left + 'px',
         }"
     >
         <q-slide-transition>
@@ -49,21 +77,30 @@
                 v-show="!hoverShowBar || overlayHovered"
             >
                 <q-icon :name="icon" />
-                <div class="title">{{ title }}</div>
+                <div class="title" @mousedown="if(canMove) beginAction($event, 'move')">{{ title }}</div>
 
-                <q-space />
+                <q-space @mousedown.native="if(canMove) beginAction($event, 'move') />
 
-                <q-btn dense flat icon="minimize" />
-                <q-btn dense flat icon="crop_square" />
-                <q-btn dense flat icon="close" />
+                <q-btn dense flat :icon="windowStatus=='minimized'?'flip_to_front':'minimize'" @click="$emit('overlay-action', 'minimize')" />
+                <q-btn dense flat :icon="windowStatus=='maximized'?'flip_to_front':'crop_square'" @click="$emit('overlay-action', 'maximize')" />
+                <q-btn dense flat icon="close" @click="$emit('overlay-action', 'close') />
             </q-bar>
         </q-slide-transition>
 
         <q-card-section
-            class="full-height q-pa-none"
+            class="col q-pa-none"
         >
             <slot />
         </q-card-section>
+
+        <div v-if="canResize && canResizeHeight" class="resize resize-top" @mousedown="beginAction($event, 'resize-top')" />
+        <div v-if="canResize && canResizeHeight" class="resize resize-bottom" @mousedown="beginAction($event, 'resize-bottom')" />
+        <div v-if="canResize && canResizeWidth" class="resize resize-left" @mousedown="beginAction($event, 'resize-left')" />
+        <div v-if="canResize && canResizeWidth" class="resize resize-right" @mousedown="beginAction($event, 'resize-right')" />
+        <div v-if="canResize && canResizeWidth && canResizeWidth" class="resize resize-nw" @mousedown="beginAction($event, 'resize-nw')" />
+        <div v-if="canResize && canResizeWidth && canResizeWidth" class="resize resize-ne" @mousedown="beginAction($event, 'resize-ne')" />
+        <div v-if="canResize && canResizeWidth && canResizeWidth" class="resize resize-sw" @mousedown="beginAction($event, 'resize-sw')" />
+        <div v-if="canResize && canResizeWidth && canResizeWidth" class="resize resize-se" @mousedown="beginAction($event, 'resize-se')" />
     </q-card>
 </template>
 
@@ -74,53 +111,170 @@ export default {
         icon: { type: String, required: true },
         title: { type: String, required: true },
         hoverShowBar: { type: Boolean, default: false },
+
         // Dimensions
         defaultHeight: { type: Number, default: 400 },
         defaultWidth: { type: Number, default: 300 },
+        minimumHeight: { type:Number, default:50 },
+        minimumWidth: { type:Number, default:50 },
+        maximumHeight: { type:Number, default:undefined },
+        maximumWidth: { type:Number, default:undefined },
+
         // Positioning
-        position: { type: String, default: 'absolute' },
-        top: { type: String, default: '200' },
-        bottom: { type: String },
-        right: { type: String },
-        left: { type: String, default: '400' },
-        // Styling
-        background: { type: String, default: 'unset' },
-        border: { type: String, default: 'unset' },
-        boxShadow: { type: String, default: 'unset' }
+        defaultTop: { type: Number, default: 200 },
+        defaultLeft: { type: Number, default: 400 },
+        minimumExposure: { type:Number, default:100 },
+
+        // Behavior
+        canMove: { type:Boolean, default:true },
+        canResize: { type:Boolean, default:true },
+        canResizeWidth: { type:Boolean, default:true },
+        canResizeHeight: { type:Boolean, default:true },
+
+        // info from parent/manager
+        windowStatus: { type: String, default: 'restored' },
+        parentSize: { type:Object, required:true },
     },
 
     data () {
         return {
             // Settings and Properties
-            overlayHeight: this.defaultHeight,
-            overlayWidth: this.defaultWidth,
-            overlayPosition: this.position,
-            overlayTop: this.top,
-            overlayBottom: this.bottom,
-            overlayRight: this.right,
-            overlayLeft: this.left,
-            overlayBackground: this.background,
-            overlayBorder: this.border,
-            overlayBoxShadow: this.boxShadow,
+            height: this.defaultHeight,
+            width: this.defaultWidth,
+            top: this.defaultTop,
+            left: this.defaultLeft,
+            
             // Internal
-            overlayHovered: false
+            hovered: false,
+            dragAction: undefined,
+            dragStart: undefined,
+            mouseCaptured: false,
         };
+    },
+
+    watch: {
+        mouseCaptured(newVal) {
+            if(newVal) {
+                window.addEventListener('mousemove', this.onDragMove, true);
+                window.addEventListener('mouseup', this.onDragDone, true);
+            } else {
+                window.removeEventListener('mousemove', this.onDragMove, true);
+                window.removeEventListener('mouseup', this.onDragDone, true);
+            }
+        },
     },
 
     computed: {
     },
 
     methods: {
-        onHoverStart () {
-            this.overlayHovered = true;
+        dragBehavior(action) {
+            let top = 0;
+            let left = 0;
+            let width = 0;
+            let height = 0;
+            switch(action) {
+                case 'move':
+                    top = +1;
+                    left = +1;
+                    break;
+                case 'resize-top':
+                    top = +1;
+                    height = -1;
+                    break;
+                case 'resize-bottom':
+                    height = +1;
+                    break;
+                case 'resize-left':
+                    left = +1;
+                    width = -1;
+                    break;
+                case 'resize-right':
+                    width = +1;
+                    break;
+                case 'resize-nw':
+                    top = +1;
+                    left = +1;
+                    height = -1;
+                    width = -1;
+                    break;
+                case 'resize-ne':
+                    top = +1;
+                    height = -1;
+                    width = +1;
+                    break;
+                case 'resize-sw':
+                    left = +1;
+                    width = -1;
+                    height = +1;
+                    break;
+                case 'resize-se':
+                    height = +1;
+                    width = +1;
+                    break;
+            }
+            return {top:top,left:left,width:width,height:height};
         },
 
-        onHoverEnd () {
-            this.overlayHovered = false;
-        }
+        beginAction(event, action) {
+            this.dragAction = action;
+            this.dragStart = {x:event.screenX,y:event.screenY};
+            this.mouseCaptured = true;
+        },
+
+        applyDrag(pageX, pageY) {
+            if(!this.dragAction || !this.dragStart) return; // shouldn't be here, get out now
+            const behavior = this.dragBehavior(this.action);
+            const offsetX = pageX - this.dragStart.x;
+            const offsetY = pageY - this.dragStart.y;
+
+            // apply the size changes first and enforce min/max sizes
+            let newWidth = this.width + offsetX * behavior.width;
+            if(this.minimumWidth && newWidth < this.minimumWidth) {
+                newWidth = this.minimumWidth;
+                offsetX = (newWidth - this.width) * behavior.width;
+            }
+            if(this.maximumWidth && newWidth > this.maximumWidth) {
+                newWidth = this.maximumWidth;
+                offsetX = (newWidth - this.width) * behavior.width;
+            }
+
+            let newHeight = this.height + offsetY * behavior.height;
+            if(this.minimumHeight && newHeight < this.minimumHeight) {
+                newHeight = this.minimumHeight;
+                offsetX = (newHeight - this.height) * behavior.height;
+            }
+            if(this.maximumHeight && newHeight > this.maximumHeight) {
+                newHeight = this.maximumHeight;
+                offsetX = (newHeight - this.height) * behavior.height;
+            }
+
+            this.width = newWidth;
+            this.height = newHeight;
+            this.top += offsetY * behavior.top;
+            this.left += offsetX * behavior.left;
+        },
+
+        onDragMove(event) {
+            this.applyDrag(event.screenX, event.screenY);
+        },
+
+        onDragDone(event) {
+            this.applyDrag(event.screenX, event.screenY);
+            this.mouseCaptured = false;
+            this.dragAction = undefined;
+            this.dragStart = undefined;
+        },
     },
 
     mounted () {
-    }
+    },
+
+    destroyed() {
+        if(this.mouseCaptured) {
+            window.removeEventListener('mousemove', this.onDragMove, true);
+            window.removeEventListener('mouseup', this.onDragDone, true);
+        }
+    },
 };
 </script>
